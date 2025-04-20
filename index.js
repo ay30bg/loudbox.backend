103c297a1fed93e55fcde6530c9848f55d95d504
ay30bg <117005347+ay30bg@users.noreply.github.com>
2025-04-20 12:25:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const app = express();
 
 // Configure CORS
 const allowedOrigins = [
-  'http://localhost:3000', 'https://loudbox.vercel.app',
+  'http://localhost:3000', 
+  process.env.FRONTEND_URL || 'https://loudbox.vercel.app',
 ];
 
 const corsOptions = {
