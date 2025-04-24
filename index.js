5659035380eafe1a9833f8c2cc12853b19dff81e
ay30bg <117005347+ay30bg@users.noreply.github.com>
2025-04-24 13:33:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const ticketRoutes = require('./routes/tickets');
 const verifyRoutes = require('./routes/verify');
+const verifyPaymentRoutes = require('./routes/verifyPayment'); // Add new route
 
 
 dotenv.config();
@@ -28,6 +29,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/', authRoutes);
 app.use('/api/tickets', ticketRoutes);
 app.use('/api/verify', verifyRoutes);
+app.use('/api/verify-payment', verifyPaymentRoutes); // Mount new route
 
 app.get('/', (req, res) => {
   res.json({ message: 'Loudbox API' });
